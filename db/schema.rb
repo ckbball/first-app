3609589a92019300ad8af560a71065585ec34c0d
caesar <caesar.ladion@gmail.com>
2023-07-29 14:22:59 -0700

    stopped at 7.4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_29_193857) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_29_210448) do
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "cars", force: :cascade do |t|
     t.string "make"
     t.string "model"
